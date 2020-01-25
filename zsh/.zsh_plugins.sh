c35cca6b6c2859c630c9aa17ac97a66bbafcc824
Matthew McDole <simucal@gmail.com>
2020-01-25 14:40:17 +0000

    add output of antibody

---
+++ zsh/.zsh_plugins.sh
@@ -0,0 +1,12 @@
+source /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc/mkc.plugin.zsh
+fpath+=( /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc )
+source /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jreese-SLASH-zsh-titles/titles.plugin.zsh
+fpath+=( /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jreese-SLASH-zsh-titles )
+source /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-agkozak-zsh-prompt/agkozak-zsh-prompt.plugin.zsh
+fpath+=( /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-agkozak-zsh-prompt )
+source /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/drake/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
